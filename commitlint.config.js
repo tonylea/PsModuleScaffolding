eeaa54570daef394d4afbf2d44ffca817fcc1566
Tony Lea <tonylea@deadrobot.co.uk>
2021-08-21 17:03:19 +0000

    chore: update commitlint rules
    Ignore scope empty rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,14 @@
 const conventionalCommit = require("./conventionalCommit.json");
 
 const typesEnum = Object.keys(conventionalCommit.types);
+const scopesEnum = Object.keys(conventionalCommit.scopes);
 
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [2, "always", typesEnum],
-    "scope-case": [2, "always", ["camel-case"]]
+    "scope-case": [2, "always", ["camel-case"]],
+    "scope-enum": [2, "always", scopesEnum],
+    "scope-empty": [0, "always"]
   },
 };
